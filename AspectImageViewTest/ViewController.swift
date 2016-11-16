789ad140857e6e6cf3c3be691e7e3c16eb686c27
Charlie Hieger <charlie@codepath.com>
2016-11-16 12:19:39 -0800

    Working version of FlexibleAspectImageView class

--- AspectImageViewTest/ViewController.swift
+++ AspectImageViewTest/ViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var aspectImageView: FlexibleAspectImageView!
+
+    var originalFrame: CGRect!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        originalFrame = aspectImageView.frame
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +23,27 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func slider(_ sender: UISlider) {
 
+        let value = CGFloat(sender.value)
+
+        aspectImageView.frame.size.width = originalFrame.size.width + value
+    }
+
+
+    @IBAction func didAdjustHeight(_ sender: UISlider) {
+        let value = CGFloat(sender.value)
+
+        aspectImageView.frame.size.height = originalFrame.size.height + value
+
+    }
+
+    @IBAction func didAdjustOrigin(_ sender: UISlider) {
+        let value = CGFloat(sender.value)
+
+        aspectImageView.frame.origin = CGPoint(x: originalFrame.origin.x + value, y: originalFrame.origin.y + value)
+
+
+    }
 }
 
